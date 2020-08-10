bdf76c66b0932d94a8375bc1e7a7de1f57e427b5
geoffroyleconte <gleconte50@gmail.com>
2020-08-10 14:02:32 +0200

    test2

--- amdahl_benchmarks/mehrotra_benchmark.jl
+++ amdahl_benchmarks/mehrotra_benchmark.jl
@@ -355,7 +355,7 @@ end
 
 
 function mehrotraPCQuadBounds(QM0; max_iter=200, ϵ_pdd=1e-8, ϵ_rb=1e-6, ϵ_rc=1e-6,
-                              tol_Δx=1e-16, ϵ_μ=1e-9, max_time=1000., scaling=true,
+                              tol_Δx=1e-16, ϵ_μ=1e-9, max_time=1300., scaling=true,
                               display=true)
 
     start_time = time()
