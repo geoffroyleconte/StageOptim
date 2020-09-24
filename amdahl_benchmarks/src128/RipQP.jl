e99c281efe0d07f94888cf397ea3151bdf9f0dba
Geoffroy Leconte <gleconte50@gmail.com>
2020-09-24 15:37:50 +0200

    test 128

---
+++ amdahl_benchmarks/src128/RipQP.jl
@@ -0,0 +1,267 @@
+module RipQP
+
+using LinearAlgebra, SparseArrays, Statistics, Quadmath
+
+using LDLFactorizations, NLPModels, QuadraticModels, SolverTools
+
+export ripqp
+
+include("starting_points.jl")
+include("scaling.jl")
+include("sparse_toolbox.jl")
+include("iterations.jl")
+include("types_toolbox.jl")
+
+function ripqp(QM0; mode = :mono, max_iter=800, ϵ_pdd=1e-8, ϵ_rb=1e-6, ϵ_rc=1e-6,
+               tol_Δx=1e-16, ϵ_μ=1e-9, max_time=1200., scaling=true, display=true)
+
+    if mode ∉ [:mono, :multi]
+        error("mode should be :mono or :multi")
+    end
+    start_time = time()
+    elapsed_time = 0.0
+    QM = SlackModel(QM0)
+
+    # get variables from QuadraticModel
+    lvar, uvar = QM.meta.lvar, QM.meta.uvar
+    n_cols = length(lvar)
+    Oc = zeros(n_cols)
+    ilow, iupp = [QM.meta.ilow; QM.meta.irng], [QM.meta.iupp; QM.meta.irng] # finite bounds index
+    n_low, n_upp = length(ilow), length(iupp) # number of finite constraints
+    irng = QM.meta.irng
+    ifix = QM.meta.ifix
+    c = grad(QM, Oc)
+    A = jac(QM, Oc)
+    A = dropzeros!(A)
+    Arows, Acols, Avals = findnz(A)
+    n_rows, n_cols = size(A)
+    @assert QM.meta.lcon == QM.meta.ucon # equality constraint (Ax=b)
+    b = QM.meta.lcon
+    Q = hess(QM, Oc)  # lower triangular
+    Q = dropzeros!(Q)
+    Qrows, Qcols, Qvals = findnz(Q)
+    c0 = obj(QM, Oc)
+    Qvals, Avals, c, c0, b,
+        lvar, uvar = convert_data(Float128, Qvals, Avals, c, c0, b, lvar, uvar)
+    T0 = eltype(Avals)
+    T = T0
+
+    if scaling
+        Arows, Acols, Avals, Qrows, Qcols, Qvals,
+        c, b, lvar, uvar, d1, d2, d3 = scaling_Ruiz!(Arows, Acols, Avals, Qrows, Qcols, Qvals,
+                                                     c, b, lvar, uvar, n_rows, n_cols, T(1.0e-3))
+    end
+    # cNorm = norm(c)
+    # bNorm = norm(b)
+    # ANorm = norm(Avals)  # Frobenius norm after scaling; could be computed while scaling?
+    # QNorm = norm(Qvals)
+
+    if mode == :multi
+        #change types
+        T = Float32
+        Qvals32, Avals32, c32, c032, b32,
+            lvar32, uvar32, ϵ_pdd32, ϵ_rb32, ϵ_rc32,
+            tol_Δx32, ϵ_μ32, ρ, δ, ρ_min, δ_min, tmp_diag,
+            J_augm, diagind_J, diag_Q, x_m_l_αΔ_aff,
+            u_m_x_αΔ_aff, s_l_αΔ_aff, s_u_αΔ_aff, rxs_l,
+            rxs_u, Δ_aff, Δ_cc, Δ, Δ_xλ, x, λ, s_l, s_u,
+            J_fact, J_P, Qx, ATλ, Ax, x_m_lvar, uvar_m_x,
+            xTQx_2,  cTx, pri_obj, dual_obj, μ, pdd,
+            rc, rb, rcNorm, rbNorm, tol_rb32, tol_rc32,
+            tol_rb, tol_rc, optimal, small_Δx, small_μ,
+            l_pdd, mean_pdd, n_Δx = init_params(T, Qrows, Qcols, Qvals,  Arows, Acols, Avals,
+                                                c, c0, b, lvar, uvar, tol_Δx, ϵ_μ, ϵ_rb, ϵ_rc,
+                                                n_rows, n_cols, ilow, iupp, irng, n_low, n_upp)
+
+    elseif mode == :mono
+        # init regularization values
+        ρ, δ, ρ_min, δ_min, tmp_diag, J_augm,
+            diagind_J, diag_Q, x_m_l_αΔ_aff,
+            u_m_x_αΔ_aff, s_l_αΔ_aff, s_u_αΔ_aff,
+            rxs_l, rxs_u, Δ_aff, Δ_cc, Δ, Δ_xλ,
+            x, λ, s_l, s_u, J_fact, J_P, Qx, ATλ, Ax,
+            x_m_lvar, uvar_m_x, xTQx_2,  cTx,
+            pri_obj, dual_obj, μ, pdd, rc, rb,
+            rcNorm, rbNorm, tol_rb, tol_rc, optimal,
+            small_Δx, small_μ,
+            l_pdd, mean_pdd, n_Δx = init_params_mono(Qrows, Qcols, Qvals,  Arows, Acols, Avals,
+                                                     c, c0, b, lvar, uvar, tol_Δx, ϵ_pdd, ϵ_μ,
+                                                     ϵ_rb, ϵ_rc, n_rows, n_cols, ilow, iupp, irng,
+                                                     n_low, n_upp)
+    end
+
+    Δt = time() - start_time
+    tired = Δt > max_time
+    k = 0
+    c_catch = zero(Int) # to avoid endless loop
+    c_pdd = zero(Int) # avoid too small δ_min
+
+    # display
+    if display == true
+        @info log_header([:k, :pri_obj, :pdd, :rbNorm, :rcNorm, :n_Δx, :α_pri, :α_du, :μ],
+        [Int, T, T, T, T, T, T, T, T, T],
+        hdr_override=Dict(:k => "iter", :pri_obj => "obj", :pdd => "rgap",
+        :rbNorm => "‖rb‖", :rcNorm => "‖rc‖",
+        :n_Δx => "‖Δx‖"))
+        @info log_row(Any[k, pri_obj, pdd, rbNorm, rcNorm, n_Δx, zero(T), zero(T), μ])
+    end
+
+    if mode == :multi
+        # iters Float 32
+        x, λ, s_l, s_u, x_m_lvar, uvar_m_x,
+            rc, rb, rcNorm, rbNorm, Qx, ATλ,
+            Ax, xTQx_2, cTx, pri_obj, dual_obj,
+            pdd, l_pdd, mean_pdd, n_Δx, Δt,
+            tired, optimal, μ, k, ρ, δ,
+            ρ_min, δ_min, J_augm, J_fact,
+            c_catch, c_pdd  = iter_mehrotraPC!(T0, x, λ, s_l, s_u, x_m_lvar, uvar_m_x, lvar32, uvar32,
+                                               ilow, iupp, n_rows, n_cols,n_low, n_upp,
+                                               Arows, Acols, Avals32, Qrows, Qcols, Qvals32, c032,
+                                               c32, b32, rc, rb, rcNorm, rbNorm, tol_rb32, tol_rc32,
+                                               Qx, ATλ, Ax, xTQx_2, cTx, pri_obj, dual_obj,
+                                               pdd, l_pdd, mean_pdd, n_Δx, small_Δx, small_μ,
+                                               Δt, tired, optimal, μ, k, ρ, δ, ρ_min, δ_min,
+                                               J_augm, J_fact, J_P, diagind_J, diag_Q, tmp_diag,
+                                               Δ_aff, Δ_cc, Δ, Δ_xλ, s_l_αΔ_aff, s_u_αΔ_aff,
+                                               x_m_l_αΔ_aff, u_m_x_αΔ_aff, rxs_l, rxs_u,
+                                               100, ϵ_pdd32, ϵ_μ32, ϵ_rc32, ϵ_rb32, tol_Δx32,
+                                               start_time, max_time, c_catch, c_pdd, display)
+
+        # conversions to Float64
+        T = Float64
+        x, λ, s_l, s_u, x_m_lvar,
+            uvar_m_x, rc, rb,
+            rcNorm, rbNorm, Qx,
+            ATλ, Ax, xTQx_2, cTx,
+            pri_obj, dual_obj, pdd,
+            l_pdd, mean_pdd, n_Δx,
+            μ, ρ, δ, J_augm, J_P,
+            J_fact, Δ_aff, Δ_cc, Δ,
+            Δ_xλ, rxs_l, rxs_u, s_l_αΔ_aff,
+            s_u_αΔ_aff, x_m_l_αΔ_aff,
+            u_m_x_αΔ_aff, diag_Q,
+            tmp_diag, ρ_min, δ_min = convert_types!(T, x, λ, s_l, s_u, x_m_lvar, uvar_m_x,
+                                                    rc, rb,rcNorm, rbNorm, Qx, ATλ, Ax,
+                                                    xTQx_2, cTx, pri_obj, dual_obj, pdd,
+                                                    l_pdd, mean_pdd, n_Δx, μ, ρ, δ,
+                                                    J_augm, J_P, J_fact, Δ_aff, Δ_cc, Δ,
+                                                    Δ_xλ, rxs_l, rxs_u, s_l_αΔ_aff,
+                                                    s_u_αΔ_aff, x_m_l_αΔ_aff, u_m_x_αΔ_aff,
+                                                    diag_Q, tmp_diag, ρ_min, δ_min)
+
+        optimal = pdd < ϵ_pdd && rbNorm < tol_rb && rcNorm < tol_rc
+        small_Δx, small_μ = n_Δx < tol_Δx, μ < ϵ_μ
+        ρ /= 10
+        δ /= 10
+
+        if T0 == Float128 # iters 64
+            Qvals64, Avals64, c64, c064,
+                b64, lvar64, uvar64 = convert_data(T, Qvals, Avals, c, c0, b,
+                                                    lvar, uvar)
+            ϵ_pdd64, ϵ_rb64, ϵ_rc64 = T(1e-3), T(1e-3), T(1e-3)
+            tol_rb64, tol_rc64 = ϵ_rb64*(one(T) + rbNorm), ϵ_rc64*(one(T) + rcNorm)
+            ρ_min, δ_min = T(sqrt(eps(T))*1e0), T(sqrt(eps(T))*1e0)
+            ϵ_μ64, tol_Δx64 = T(1e-10), T(1e-20)
+
+            x, λ, s_l, s_u, x_m_lvar, uvar_m_x,
+                rc, rb, rcNorm, rbNorm, Qx, ATλ,
+                Ax, xTQx_2, cTx, pri_obj, dual_obj,
+                pdd, l_pdd, mean_pdd, n_Δx, Δt,
+                tired, optimal, μ, k, ρ, δ,
+                ρ_min, δ_min, J_augm, J_fact,
+                c_catch, c_pdd  = iter_mehrotraPC!(T0, x, λ, s_l, s_u, x_m_lvar, uvar_m_x, lvar64, uvar64,
+                                                   ilow, iupp, n_rows, n_cols,n_low, n_upp,
+                                                   Arows, Acols, Avals64, Qrows, Qcols, Qvals64, c064,
+                                                   c64, b64, rc, rb, rcNorm, rbNorm, tol_rb64, tol_rc64,
+                                                   Qx, ATλ, Ax, xTQx_2, cTx, pri_obj, dual_obj,
+                                                   pdd, l_pdd, mean_pdd, n_Δx, small_Δx, small_μ,
+                                                   Δt, tired, optimal, μ, k, ρ, δ, ρ_min, δ_min,
+                                                   J_augm, J_fact, J_P, diagind_J, diag_Q, tmp_diag,
+                                                   Δ_aff, Δ_cc, Δ, Δ_xλ, s_l_αΔ_aff, s_u_αΔ_aff,
+                                                   x_m_l_αΔ_aff, u_m_x_αΔ_aff, rxs_l, rxs_u,
+                                                   1000, ϵ_pdd64, ϵ_μ64, ϵ_rc64, ϵ_rb64, tol_Δx64,
+                                                   start_time, max_time, c_catch, c_pdd, display)
+            # conversions to Float128
+            T = Float128
+            x, λ, s_l, s_u, x_m_lvar,
+               uvar_m_x, rc, rb,
+               rcNorm, rbNorm, Qx,
+               ATλ, Ax, xTQx_2, cTx,
+               pri_obj, dual_obj, pdd,
+               l_pdd, mean_pdd, n_Δx,
+               μ, ρ, δ, J_augm, J_P,
+               J_fact, Δ_aff, Δ_cc, Δ,
+               Δ_xλ, rxs_l, rxs_u, s_l_αΔ_aff,
+               s_u_αΔ_aff, x_m_l_αΔ_aff,
+               u_m_x_αΔ_aff, diag_Q,
+               tmp_diag, ρ_min, δ_min = convert_types!(T, x, λ, s_l, s_u, x_m_lvar, uvar_m_x,
+                                                       rc, rb,rcNorm, rbNorm, Qx, ATλ, Ax,
+                                                       xTQx_2, cTx, pri_obj, dual_obj, pdd,
+                                                       l_pdd, mean_pdd, n_Δx, μ, ρ, δ,
+                                                       J_augm, J_P, J_fact, Δ_aff, Δ_cc, Δ,
+                                                       Δ_xλ, rxs_l, rxs_u, s_l_αΔ_aff,
+                                                       s_u_αΔ_aff, x_m_l_αΔ_aff, u_m_x_αΔ_aff,
+                                                       diag_Q, tmp_diag, ρ_min, δ_min)
+
+            optimal = pdd < ϵ_pdd && rbNorm < tol_rb && rcNorm < tol_rc
+            small_Δx, small_μ = n_Δx < tol_Δx, μ < ϵ_μ
+            ρ /= 10
+            δ /= 10
+        end
+    end
+
+    ρ_min, δ_min = T(sqrt(eps(T0))*1e0), T(sqrt(eps(T0))*1e0)
+    # iters T0
+    x, λ, s_l, s_u, x_m_lvar, uvar_m_x,
+        rc, rb, rcNorm, rbNorm, Qx, ATλ,
+        Ax, xTQx_2, cTx, pri_obj, dual_obj,
+        pdd, l_pdd, mean_pdd, n_Δx, Δt,
+        tired, optimal, μ, k, ρ, δ,
+        ρ_min, δ_min, J_augm, J_fact,
+        c_catch, c_pdd  = iter_mehrotraPC!(T0, x, λ, s_l, s_u, x_m_lvar, uvar_m_x, lvar, uvar,
+                                           ilow, iupp, n_rows, n_cols,n_low, n_upp,
+                                           Arows, Acols, Avals, Qrows, Qcols, Qvals, c0,
+                                           c, b, rc, rb, rcNorm, rbNorm, tol_rb, tol_rc,
+                                           Qx, ATλ, Ax, xTQx_2, cTx, pri_obj, dual_obj,
+                                           pdd, l_pdd, mean_pdd, n_Δx, small_Δx, small_μ,
+                                           Δt, tired, optimal, μ, k, ρ, δ, ρ_min, δ_min,
+                                           J_augm, J_fact, J_P, diagind_J, diag_Q, tmp_diag,
+                                           Δ_aff, Δ_cc, Δ, Δ_xλ, s_l_αΔ_aff, s_u_αΔ_aff,
+                                           x_m_l_αΔ_aff, u_m_x_αΔ_aff, rxs_l, rxs_u,
+                                           max_iter, ϵ_pdd, ϵ_μ, ϵ_rc, ϵ_rb, tol_Δx,
+                                           start_time, max_time, c_catch, c_pdd, display)
+
+
+    if k>= max_iter
+        status = :max_iter
+    elseif tired
+        status = :max_time
+    elseif optimal
+        status = :acceptable
+    else
+        status = :unknown
+    end
+
+    if scaling
+        x, λ, s_l, s_u, pri_obj,
+            rcNorm, rbNorm = post_scale(d1, d2, d3, x, λ, s_l, s_u, rb, rc, rcNorm,
+                                        rbNorm, lvar, uvar,ilow, iupp, b, c, c0,
+                                        Qrows, Qcols, Qvals, Arows, Acols, Avals,
+                                        Qx, ATλ, Ax, cTx, pri_obj, dual_obj, xTQx_2)
+    end
+
+    elapsed_time = time() - start_time
+
+    stats = GenericExecutionStats(status, QM, solution = x[1:QM.meta.nvar],
+                                  objective = pri_obj,
+                                  dual_feas = rcNorm,
+                                  primal_feas = rbNorm,
+                                  multipliers = λ,
+                                  multipliers_L = s_l,
+                                  multipliers_U = s_u,
+                                  iter = k,
+                                  elapsed_time=elapsed_time)
+    return stats
+end
+
+end
---
+++ amdahl_benchmarks/test_quadLP.jl
@@ -0,0 +1,127 @@
+using QuadraticModels, QPSReader
+using Quadmath
+# path_pb = "C:\\Users\\Geoffroy Leconte\\Documents\\cours\\TFE\\code\\quadLP\\data\\MPS"
+path_pb = "/home/mgi.polymtl.ca/geleco/quad_optim/problems/quadLP/data/MPS"
+
+function rm_ifix!(ifix, Qrows, Qcols, Qvals, c, c0, Arows, Acols, Avals,
+                  Lcon, Ucon, lvar, uvar, n_rows, n_cols)
+    T = eltype(c)
+    # get Qii
+    diag_Q = zeros(T, n_cols)
+    for i=1:length(Qvals)
+        if Qrows[i] == Qcols[i]
+            diag_Q[Qrows[i]] = Qvals[i]
+        end
+    end
+    ifix = sort!(ifix)
+    # update c0, c
+    Qji = zero(T)
+    for i=1:length(ifix)
+        c0 += c[ifix[i]] * lvar[ifix[i]] + diag_Q[ifix[i]] * lvar[ifix[i]]^2 / 2
+        for j=1:n_cols
+            Qji = zero(T)
+            for k=1:length(Qvals)
+                if (Qrows[k] == i && Qcols[k] == j) || (Qrows[k] == j && Qcols[k] == i)
+                    Qji += Qvals[k]
+                end
+            end
+            c[j] += lvar[ifix[i]] * Qji
+        end
+    end
+
+    # remove columns in ifix
+    ifix_cols_A = findall(x->x in ifix, Acols)
+    ifix_cols_A = sort!(ifix_cols_A)
+    for i=1:length(Acols)
+        if i in ifix_cols_A
+            Lcon[Arows[i]] -= Avals[i] * lvar[Acols[i]]
+            Ucon[Arows[i]] -= Avals[i] * lvar[Acols[i]]
+        end
+    end
+    Arows_rm_fix = Arows[ifix_cols_A]
+    Acols_rm_fix = Acols[ifix_cols_A]
+    Avals_rm_fix = Avals[ifix_cols_A]
+    Arows = deleteat!(Arows, ifix_cols_A)
+    Acols = deleteat!(Acols, ifix_cols_A)
+    Avals = deleteat!(Avals, ifix_cols_A)
+
+    for i=1:length(Acols)
+        if Acols[i] > ifix[1]
+            Acols[i] -= findlast(ifix .<= Acols[i])
+        end
+    end
+    # remove rows and columns in ifix
+    ifix_cols_Q = findall(x-> x in ifix, Qcols)
+
+    Q_rm_idx = [] #unique(hcat(ifix_rows_Q, ifix_cols_Q))
+    Qrows_rm_fix = Qrows[Q_rm_idx]
+    Qcols_rm_fix = Qcols[Q_rm_idx]
+    Qvals_rm_fix = Qvals[Q_rm_idx]
+    for i=1:length(ifix_cols_Q)
+        Qrows_rm_fix = push!(Qrows_rm_fix, splice!(Qrows, ifix_cols_Q[i]-i+1))
+        Qcols_rm_fix = push!(Qcols_rm_fix, splice!(Qcols, ifix_cols_Q[i]-i+1))
+        Qvals_rm_fix = push!(Qvals_rm_fix, splice!(Qvals, ifix_cols_Q[i]-i+1))
+    end
+    ifix_rows_Q = findall(x-> x in ifix, Qrows)
+    for i=1:length(ifix_rows_Q)
+        Qrows_rm_fix = push!(Qrows_rm_fix, splice!(Qrows, ifix_rows_Q[i]-i+1))
+        Qcols_rm_fix = push!(Qcols_rm_fix, splice!(Qcols, ifix_rows_Q[i]-i+1))
+        Qvals_rm_fix = push!(Qvals_rm_fix, splice!(Qvals, ifix_rows_Q[i]-i+1))
+    end
+
+    for i=1:length(Qcols)
+        if  Qrows[i] > ifix[1]
+            Qrows[i] -= findlast(ifix .<= Qrows[i])
+        end
+        if Qcols[i] > ifix[1]
+            Qcols[i] -= findlast(ifix .<= Qcols[i])
+        end
+    end
+
+    c_rm_fix = c[ifix]
+    x_rm_fix = lvar[ifix]
+    c = deleteat!(c, ifix)
+    lvar = deleteat!(lvar, ifix)
+    uvar = deleteat!(uvar, ifix)
+    n_cols -= length(ifix)
+
+    return Qrows, Qcols, Qvals, c, c0, Arows, Acols, Avals, Lcon, Ucon,
+            lvar, uvar, n_cols, Arows_rm_fix, Acols_rm_fix, Avals_rm_fix,
+            Qrows_rm_fix, Qcols_rm_fix, Qvals_rm_fix, c_rm_fix, x_rm_fix, ifix
+end
+
+function createQuadraticModel128(qpdata; name="qp_pb")
+    return QuadraticModel(convert(Array{Float128}, qps1.c), qpdata.qrows, qpdata.qcols,
+            convert(Array{Float128}, qps1.qvals),
+            Arows=qpdata.arows, Acols=qpdata.acols,
+            Avals=convert(Array{Float128}, qps1.avals),
+            lcon=convert(Array{Float128}, qps1.lcon),
+            ucon=convert(Array{Float128}, qps1.ucon),
+            lvar=convert(Array{Float128}, qps1.lvar),
+            uvar=convert(Array{Float128}, qps1.uvar),
+            c0=qpdata.c0, name=name)
+end
+
+qps1 = readqps(string(path_pb, "\\TMA_ME.mps"))
+qps1.qrows, qps1.qcols, qps1.qvals,
+    qps1.c, qps1.c0, qps1.arows,
+    qps1.acols, qps1.avals, qps1.lcon,
+    qps1.ucon, qps1.lvar, qps1.uvar,
+    qps1.nvar,
+    Arows_rm_fix, Acols_rm_fix, Avals_rm_fix,
+    Qrows_rm_fix, Qcols_rm_fix, Qvals_rm_fix,
+    c_rm_fix, x_rm_fix, ifix = rm_ifix!(findall(qps1.uvar .== qps1.lvar),
+                                        qps1.qrows, qps1.qcols, qps1.qvals,
+                                        qps1.c, qps1.c0, qps1.arows, qps1.acols,
+                                        qps1.avals, qps1.lcon, qps1.ucon, qps1.lvar,
+                                        qps1.uvar, qps1.ncon, qps1.nvar)
+qm1 = createQuadraticModel128(qps1)
+
+# include(raw"C:\Users\Geoffroy Leconte\Documents\cours\TFE\code\StageOptim\amdahl_benchmarks\src128\RipQP.jl")
+include("/home/mgi.polymtl.ca/geleco/git_workspace/StageOptim/amdahl_benchmarks/src128/RipQP.jl")
+
+stats1 = RipQP.ripqp(qm1, mode=:multi, max_iter=20000)
+println(stats1)
+
+# qps2 = readqps(string(path_pb, "\\GlcAerWT.mps"))
+# qm2 = QuadraticModel(qps1)
