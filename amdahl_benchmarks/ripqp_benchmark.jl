aaa947aecedeef645b8a71a313963defc5856a8a
Geoffroy Leconte <gleconte50@gmail.com>
2021-02-04 22:08:21 -0500

    reduce time

--- amdahl_benchmarks/ripqp_benchmark.jl
+++ amdahl_benchmarks/ripqp_benchmark.jl
@@ -22,7 +22,7 @@ qm = createQuadraticModel(qpdata)
 stats =  ripqp(qm, mode=:mono, regul=:classic, K=0)  # compile code
 
 function ripqp_bm(QM)
-    return ripqp(QM, mode=:mono, regul=:classic, K=0, max_time=1200.)
+    return ripqp(QM, mode=:mono, regul=:classic, K=0, max_time=12.)
 end
 
 function optimize_ripqp(path_pb)
