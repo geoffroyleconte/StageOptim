7da7ddd1d15d709b6d42c2474831cd8df6158942
Geoffroy Leconte <gleconte50@gmail.com>
2021-04-13 14:42:33 -0400

    bm multipliers

--- amdahl_benchmarks/ripqp_benchmark.jl
+++ amdahl_benchmarks/ripqp_benchmark.jl
@@ -20,9 +20,9 @@ qpdata = readqps(pb);
 qm = createQuadraticModel(qpdata)
 stats =  ripqp(qm)  # compile code
 
-# ripqp_bm_classic(QM) = ripqp(QM, itol = InputTol(max_time=1200.))
+ripqp_bm_classic(QM) = ripqp(QM, itol = InputTol(max_time=1200.))
 # ripqp_bm_ipf(QM) = ripqp(QM, itol = InputTol(max_time=1200.) , iconf = InputConfig(solve_method=:IPF))
-ripqp_bm_cc(QM) = ripqp(QM, iconf = InputConfig(kc=-1), itol = InputTol(max_time=1200.))
+# ripqp_bm_cc(QM) = ripqp(QM, iconf = InputConfig(kc=-1), itol = InputTol(max_time=1200.))
 
 function optimize_ripqp(path_pb :: String, ripqp_func :: Function)
     problems = []
@@ -92,10 +92,10 @@ function save_problems(file_path :: String, ripqp_func :: Function,
     return Nothing
 end
 
-# save_problems(string(save_path, "/ripqp_mono_2"), ripqp_bm_classic)
+save_problems(string(save_path, "/ripqp_mono_1"), ripqp_bm_classic)
 # save_problems(string(save_path, "/ripqp_mono_IPFK2_1"), ripqp_bm_ipf)
 # save_problems(string(save_path, "/ripqp_ccorr_1"), ripqp_bm_cc)
-save_problems(string(save_path, "/ripqp_ccorr_3"), ripqp_bm_cc)
+# save_problems(string(save_path, "/ripqp_ccorr_3"), ripqp_bm_cc)
 # save_problems(string(save_path, "/ripqp_multi_r"), ripqp_bm_multiref)
 # save_problems(string(save_path, "/ripqp_mono_z"), ripqp_bm_zoom)
 # save_problems(string(save_path, "/ripqp_multi_z"), ripqp_bm_multizoom)
