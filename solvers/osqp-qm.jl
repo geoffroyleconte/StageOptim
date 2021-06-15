727f658a83a4e827fc62eb23af08011bf32c7a5d
Geoffroy Leconte <gleconte50@gmail.com>
2021-06-15 18:53:48 -0400

    osqp qm interface

---
+++ solvers/osqp-qm.jl
@@ -0,0 +1,50 @@
+using OSQP, QuadraticModels, SolverCore, SparseArrays, LinearAlgebra
+
+const status_map_qm = Dict{Symbol,Symbol}(
+  :Dual_infeasible_inaccurate => :infeasible,
+  :Primal_infeasible_inaccurate => :infeasible,
+  :Solved_inaccurate => :unknown,
+  :Solved => :acceptable,
+  :Max_iter_reached => :max_iter,
+  :Primal_infeasible => :infeasible,
+  :Dual_infeasible => :infeasible,
+  :Interrupted => :user,
+  :Time_limit_reached => :max_time,
+  :Non_convex => :exception,
+  :Unsolved => :unknown)
+
+function osqp(QM::QuadraticModel; kwargs...)
+
+  m = OSQP.Model()
+  
+  H = sparse(QM.data.Hcols, QM.data.Hrows, QM.data.Hvals, QM.meta.nvar, QM.meta.nvar)
+  A = [sparse(QM.data.Arows, QM.data.Acols, QM.data.Avals, QM.meta.ncon, QM.meta.nvar); I]
+  l = [QM.meta.lcon; QM.meta.lvar]
+  u = [QM.meta.ucon; QM.meta.uvar]
+
+  OSQP.setup!(m; P=H, q=QM.data.c, A=A, l=l, u=u, kwargs...)
+  results = OSQP.solve!(m)
+
+  stats = GenericExecutionStats(
+    status_map_qm[results.info.status],
+    QM,
+    solution = results.x,
+    objective = results.info.obj_val + QM.data.c0,
+    dual_feas = results.info.dua_res,
+    primal_feas = results.info.pri_res,
+    multipliers = results.y,
+    iter = results.info.iter,
+    elapsed_time = results.info.run_time,
+    solver_specific = Dict(:setup_time => results.info.setup_time,
+                           :solve_time => results.info.solve_time,
+                           :supdate_time => results.info.update_time,
+                           :polish_time => results.info.polish_time,
+                           ),
+  )
+  return stats
+end
+
+path_pb = "C:\\Users\\Geoffroy Leconte\\Documents\\doctorat\\code\\datasets\\problemes_marosmeszaros"
+qm = QuadraticModel(readqps(string(path_pb, "\\QAFIRO.SIF"), mpsformat=:fixed))
+stats = osqp(qm)
+println(stats)
