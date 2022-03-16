489df807d517b23b44f053c58e177087332e8065
Geoffroy Leconte <gleconte50@gmail.com>
2022-03-15 23:01:34 -0400

    test2

--- amdahl_benchmarks/ripqp_benchmark.jl
+++ amdahl_benchmarks/ripqp_benchmark.jl
@@ -95,7 +95,7 @@ function save_problems(file_path :: String, ripqp_func :: Function,
     return Nothing
 end
 
-save_problems(string(save_path, "/ripqp_mono_sc1"), ripqp_bm_classic)
+save_problems(string(save_path, "/ripqp_mono_sc2"), ripqp_bm_classic)
 # save_problems(string(save_path, "\\test"), ripqp_bm_classic)
 # save_problems(string(save_path, "/ripqp_presolve_1"), ripqp_bm_presolve)
 # save_problems(string(save_path, "/ripqp_mono_IPFK2_3"), ripqp_bm_classic)
