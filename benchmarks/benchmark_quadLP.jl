ebbb42f974dc891d74a22daac34b386f71a3b047
Geoffroy Leconte <gleconte50@gmail.com>
2022-09-22 11:14:20 -0400

    bm quad

--- benchmarks/benchmark_quadLP.jl
+++ benchmarks/benchmark_quadLP.jl
@@ -58,45 +58,46 @@ ripqp_multik2(qm; T = T, Tlow = Tlow) = ripqp(qm,
   ),
   solve_method=IPF(),
   solve_method2=PC(),
-  itol = InputTol(T, max_iter = 700, max_time = 7000.0, max_iter1 = 100, ϵ_pdd1 = T(1.0e1),
+  itol = InputTol(T, max_iter = 700, max_time = 2000.0, max_iter1 = 100, ϵ_pdd1 = T(1.0e1),
     ϵ_rc1 = T(1.0e-6), ϵ_rb1 = T(1.0e-6)),
   display = true,
 )
 stats = ripqp_multik2(qm1)
 
 ripqp_multik3(qm; T = T, Tlow = Tlow) = ripqp(qm, 
-  mode = :multi,
-  early_multi_stop = false,
-  sp = K2KrylovParams{Tlow}( # solve in Float64
-    uplo = :U,
-    kmethod=:gmresir,
-    form_mat = true,
-    equilibrate = false,
-    itmax = 100,
-    mem = 10,
-    preconditioner = LDL(T = Tlow, pos = :R, warm_start = true),
-    ρ_min=1.0e-15,
-    δ_min = 1.0e-15,
-    atol_min = 1.0e-15,
-    rtol_min = 1.0e-15,
-    Tir = T,
-  ),
-    sp2 = K2KrylovParams{T}( # solve in Float128
-    uplo = :U,
-    kmethod=:gmres,
-    form_mat = true,
-    equilibrate = false,
-    itmax = 5,
-    mem = 5,
-    preconditioner = LDL(T = T, pos = :R, warm_start = true),
-    ρ_min=T(1.0e-15),
-    δ_min = T(1.0e-15),
-    atol_min = T(1.0e-16),
-    rtol_min = T(1.0e-16),
-  ),
-  solve_method=IPF(),
-  itol = InputTol(T, max_iter = 700, max_time = 7000.0, max_iter1 = 100),
-  display = true,
+mode = :multi,
+early_multi_stop = false,
+sp = K2KrylovParams{Tlow}( # solve in Float64
+  uplo = :U,
+  kmethod=:gmres,
+  form_mat = true,
+  equilibrate = false,
+  itmax = 50,
+  mem = 50,
+  preconditioner = LDL(T = Tlow, pos = :R, warm_start = true),
+  ρ_min=1.0e-15,
+  δ_min = 1.0e-15,
+  atol_min = 1.0e-16,
+  rtol_min = 1.0e-16,
+),
+  sp2 = K2KrylovParams{T}( # solve in Float128
+  uplo = :U,
+  kmethod=:gmres,
+  form_mat = true,
+  equilibrate = false,
+  itmax = 5,
+  mem = 5,
+  preconditioner = LDL(T = T, pos = :R, warm_start = true),
+  ρ_min=T(1.0e-15),
+  δ_min = T(1.0e-15),
+  atol_min = T(1.0e-16),
+  rtol_min = T(1.0e-16),
+),
+solve_method=IPF(),
+solve_method2=PC(),
+itol = InputTol(T, max_iter = 700, max_time = 2000.0, max_iter1 = 200, ϵ_pdd1 = T(1.0e1),
+  ϵ_rc1 = T(1.0e-5), ϵ_rb1 = T(1.0e-5)),
+display = true,
 )
 stats = ripqp_multik3(qm1)
 
@@ -131,7 +132,7 @@ ripqp_multik4(qm; T = T, Tlow = Tlow) = ripqp(qm,
   ),
   solve_method=IPF(),
   solve_method2=PC(),
-  itol = InputTol(T, max_iter = 700, max_time = 7000.0, max_iter1 = 200, ϵ_pdd1 = T(1.0e1),
+  itol = InputTol(T, max_iter = 700, max_time = 2000.0, max_iter1 = 200, ϵ_pdd1 = T(1.0e1),
     ϵ_rc1 = T(1.0e-6), ϵ_rb1 = T(1.0e-6)),
   display = true,
 )
@@ -167,7 +168,7 @@ ripqp_multik5(qm; T = T, Tlow = Tlow) = ripqp(qm,
     rtol_min = T(1.0e-16),
   ),
   solve_method=IPF(),
-  itol = InputTol(T, max_iter = 700, max_time = 7000.0, max_iter1 = 100),
+  itol = InputTol(T, max_iter = 700, max_time = 2000.0, max_iter1 = 100),
   display = true,
 )
 stats = ripqp_multik5(qm1)
@@ -203,7 +204,7 @@ ripqp_multik6(qm; T = T, Tlow = Tlow) = ripqp(qm,
   ),
   solve_method=IPF(),
   solve_method2=PC(),
-  itol = InputTol(T, max_iter = 700, max_time = 7000.0, max_iter1 = 100, ϵ_pdd1 = T(1.0e1),
+  itol = InputTol(T, max_iter = 700, max_time = 2000.0, max_iter1 = 100, ϵ_pdd1 = T(1.0e1),
     ϵ_rc1 = T(1.0e-6), ϵ_rb1 = T(1.0e-6)),
   display = true,
 )
@@ -214,11 +215,11 @@ ripqp_multik7(qm; T = T, Tlow = Tlow) = ripqp(qm,
   early_multi_stop = false,
   sp = K2KrylovParams{Tlow}( # solve in Float64
     uplo = :U,
-    kmethod=:gmres,
+    kmethod=:gmresir,
     form_mat = true,
     equilibrate = false,
-    itmax = 150,
-    mem = 150,
+    itmax = 100,
+    mem = 10,
     preconditioner = LDL(T = Tlow, pos = :R, warm_start = true),
     ρ_min=1.0e-15,
     δ_min = 1.0e-15,
@@ -240,7 +241,7 @@ ripqp_multik7(qm; T = T, Tlow = Tlow) = ripqp(qm,
   ),
   solve_method=IPF(),
   solve_method2=PC(),
-  itol = InputTol(T, max_iter = 700, max_time = 7000.0, max_iter1 = 100, ϵ_pdd1 = T(1.0e1),
+  itol = InputTol(T, max_iter = 700, max_time = 2000.0, max_iter1 = 200, ϵ_pdd1 = T(1.0e1),
     ϵ_rc1 = T(1.0e-6), ϵ_rb1 = T(1.0e-6)),
   display = true,
 )
@@ -250,9 +251,12 @@ ripqp_multi(qm; T = T, Tlow = Tlow) = ripqp(
   mode = :multi,
   early_multi_stop = false,
   sp = K2LDLParams{Tlow}(),
-  itol = InputTol(T, max_iter = 700, max_time = 4000.0, max_iter1 = 100),
+  itol = InputTol(T, max_iter = 700, max_time = 2000.0, max_iter1 = 100),
 )
 stats = ripqp_multi(qm1)
+
+ripqp_mono(qm; T = T) = ripqp(qm, itol = InputTol(T, max_iter = 700, max_time = 4000.0, max_iter1 = 100))
+stats = ripqp_mono(qm1)
 
 function optimize_ripqp(path_pb :: String, ripqp_func :: Function, T::DataType)
   problems = [
@@ -271,13 +275,13 @@ function save_quad_problems(file_path :: String, ripqp_func :: Function; path_pb
 end
 
 save_quad_problems(string(save_path, "/ripqp_multik2"), ripqp_multik2, T = T)
-# save_quad_problems(string(save_path, "/ripqp_multik3"), ripqp_multik3, T = T)
+save_quad_problems(string(save_path, "/ripqp_multik3"), ripqp_multik3, T = T)
 save_quad_problems(string(save_path, "/ripqp_multik4"), ripqp_multik4, T = T)
-# save_quad_problems(string(save_path, "/ripqp_multik5"), ripqp_multik5, T = T)
+save_quad_problems(string(save_path, "/ripqp_multik5"), ripqp_multik5, T = T)
 save_quad_problems(string(save_path, "/ripqp_multik6"), ripqp_multik6, T = T)
 save_quad_problems(string(save_path, "/ripqp_multik7"), ripqp_multik7, T = T)
 save_quad_problems(string(save_path, "/ripqp_multi1"), ripqp_multi, T = T)
-
+save_quad_problems(string(save_path, "/ripqp_mono1"), ripqp_mono, T = T)
 
 # multik1
 # ripqp_multik(qm; T = T, Tlow = Tlow) = ripqp(qm, 
--- scripts/tables/quad_table_ripqp_paper.jl
+++ scripts/tables/quad_table_ripqp_paper.jl
@@ -6,11 +6,65 @@ using CSV
 # using FileIO
 open_file(fname; res_path = res_path) = CSV.read(string(res_path, "\\", fname, ".csv"), DataFrame)
 
-ripqp_multik1 = open_file("ripqp_multik1_quad") # 3 sp init 
-ripqp_multik2 = open_file("ripqp_multik2_quad") # 
-ripqp_multik3 = open_file("ripqp_multik3_quad") # 1 100iter gmres ir no eq
-ripqp_multik4 = open_file("ripqp_multik4_quad") # 
-ripqp_multik5 = open_file("ripqp_multik5_quad") # 1 100 iter gmres no eq
-ripqp_multik6 = open_file("ripqp_multik6_quad") # 
-ripqp_multik7 = open_file("ripqp_multik7_quad") # 
+ripqp_multiks = [
+  open_file("ripqp_multik1_quad"), # 3 sp init 
+  open_file("ripqp_multik2_quad"), # 
+  open_file("ripqp_multik3_quad"), # 1 100iter gmres ir no eq
+  open_file("ripqp_multik4_quad"), # 
+  open_file("ripqp_multik5_quad"), # 1 100 iter gmres no eq
+  open_file("ripqp_multik6_quad"), # 
+  open_file("ripqp_multik7_quad"), #
+] 
 ripqp_multi1 = open_file("ripqp_multi1_quad")
+
+nks = length(ripqp_multiks)
+header = [
+  "time",
+  "iters",
+  "iters 64",
+  "iters 128",
+  "objective",
+  "pdd",
+  "primal feas",
+  "dual feas",
+]
+nh = length(header)
+data = Matrix{Any}(undef, nks+1, nh)
+solver_names = [string("k", i) for i in 1:nks]
+push!(solver_names, "multi")
+pb_index = 3
+for i in 1:nks
+  data[i, :] .= [
+    ripqp_multiks[i].elapsed_time[pb_index],
+    ripqp_multiks[i].iter[pb_index],
+    ripqp_multiks[i].iters_sp[pb_index],
+    ripqp_multiks[i].iters_sp2[pb_index],
+    ripqp_multiks[i].objective[pb_index],
+    ripqp_multiks[i].pdd[pb_index],
+    ripqp_multiks[i].primal_feas[pb_index],
+    ripqp_multiks[i].dual_feas[pb_index],
+  ]
+end
+data[end, :] .= [
+  ripqp_multi1.elapsed_time[pb_index],
+  ripqp_multi1.iter[pb_index],
+  ripqp_multi1.iters_sp[pb_index],
+  ripqp_multi1.iters_sp2[pb_index],
+  ripqp_multi1.objective[pb_index],
+  ripqp_multi1.pdd[pb_index],
+  ripqp_multi1.primal_feas[pb_index],
+  ripqp_multi1.dual_feas[pb_index],
+]
+
+using PrettyTables
+
+
+nh = length(header)
+pbs = ["TMA_ME", "GlcAlift", "GlcAerWT"]
+
+pretty_table(data; 
+  header = header,
+  row_names= solver_names,
+  title = pbs[pb_index],
+  # backend = Val(:latex),
+  )
