0a45f4fe8ed28ce82209a95c78ef16e25956c642
Geoffroy Leconte <gleconte50@gmail.com>
2023-04-07 17:35:36 -0400

    dont run gurobi

--- scripts/ripqp_benchmark_paper.jl
+++ scripts/ripqp_benchmark_paper.jl
@@ -4,7 +4,7 @@ save_path = "/home/gelecd/code/docGL/benchmarks/frontal22_results/ripqp_paper_bm
 run_benchmarks_solvers(
   save_path;
   run_cplex = true,
-  run_gurobi = true,
+  run_gurobi = false,
   run_xpress = false,
   run_ma57 = true,
   run_ma97 = true,
