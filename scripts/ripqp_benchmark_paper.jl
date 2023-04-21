2a1824cdc74802e858cb8a66195b6f71bf7e89b1
Geoffroy Leconte <gleconte50@gmail.com>
2023-04-20 20:27:16 -0400

    run ripqp benchmarks

--- scripts/ripqp_benchmark_paper.jl
+++ scripts/ripqp_benchmark_paper.jl
@@ -1,13 +1,13 @@
 using RipQPBenchmarks
 
 save_path = "/home/gelecd/code/docGL/benchmarks/frontal22_results/ripqp_paper_bm"
-# run_benchmarks_solvers(
-#   save_path;
-#   run_cplex = true,
-#   run_gurobi = false,
-#   run_xpress = false,
-#   run_ma57 = true,
-#   run_ma97 = true,
-# )
 
-run_benchmarks_quad(save_path)
+ripqp_all_benchmarks(
+  save_path;
+  run_cplex = true,
+  run_gurobi = false,
+  run_xpress = false,
+  run_ma57 = true,
+  run_ma97 = true,
+  plot_extension = ".pdf",
+)
