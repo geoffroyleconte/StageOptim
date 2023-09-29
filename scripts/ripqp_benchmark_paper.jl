3306a53624d80f02be3affc11d9a2f54bd6b3801
Geoffroy Leconte <gleconte50@gmail.com>
2023-09-29 01:03:44 -0400

    run all solvers

--- scripts/ripqp_benchmark_paper.jl
+++ scripts/ripqp_benchmark_paper.jl
@@ -5,8 +5,8 @@ save_path = "/home/gelecd/code/docGL/benchmarks/frontal22_results/ripqp_paper_bm
 ripqp_all_benchmarks(
   save_path;
   run_cplex = true,
-  run_gurobi = false,
-  run_xpress = false,
+  run_gurobi = true,
+  run_xpress = true,
   run_ma57 = true,
   run_ma97 = true,
   plot_extension = ".pdf",
